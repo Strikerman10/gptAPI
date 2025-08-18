f4e05a1a377db9da9c87aa1152a2c4c0d764e16a
Strikerman10 <barney.willis2@gmail.com>
2025-08-18 14:16:09 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const WORKER_URL = "https://gptapi-proxy.barney-willis2.workers.dev";
 const SYNC_URL = WORKER_URL;
-const MODEL = "gpt-5-mini";
+const MODEL = "gpt-5";
 
 let chats = [];
 let currentIndex = null;
@@ -221,4 +221,5 @@ toggleThemeBtn.addEventListener('click', () => {
   renderChatList();
   renderMessages();
 })();
+
 
