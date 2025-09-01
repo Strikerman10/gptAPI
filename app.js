5751d1599b5ed3cc1c49229da3176f9a9a478af7
Strikerman10 <barney.willis2@gmail.com>
2025-09-01 09:33:52 +0100

    Update app.js

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ document.addEventListener("DOMContentLoaded", () => {
 },
     Amoled: {
   "--color-1": "#ffdf00",  /* golden yellow for hover */
-  "--color-2": "#db9100",  /* british yellow for header */
+  "--color-2": "#e9ab17",  /* Bee yellow for header */
   "--color-3": "#ffca37",  /* light chrome yellow for Header */
   "--color-4": "#1c1c1c",  /* Very dark gray for separators/cards */
   "--color-5": "#000000",  /* True AMOLED black background */
@@ -356,6 +356,7 @@ document.addEventListener("DOMContentLoaded", () => {
   })();
 
 });
+
 
 
 
