e5915fe79915cdade0de22697c44e157b5b039fe
Strikerman10 <barney.willis2@gmail.com>
2025-09-21 05:18:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -377,7 +377,7 @@ function formatDateTime(date = new Date()) {
   const hours = String(date.getHours()).padStart(2, '0');
   const minutes = String(date.getMinutes()).padStart(2, '0');
 
-  return `${day}/${month}/${year} ${hours}:${minutes}`;
+  return `${hours}:${minutes}\n${day}/${month}/${year}`;
 }
 
   // ==========================
@@ -780,6 +780,7 @@ async function sendMessageRetry(promptText) {
   })();
 
 }); 
+
 
 
 
